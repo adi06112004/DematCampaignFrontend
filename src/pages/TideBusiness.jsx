4ac4add447242fe8938ebdbaddb3d82cab27b7dd
Aditya Prasad <adityak2541@gmail.com>
2025-03-10 10:46:01 +0530

    Update TideBusiness.jsx

--- src/pages/TideBusiness.jsx
+++ src/pages/TideBusiness.jsx
@@ -11,7 +11,7 @@ const TideBusiness = () => {
   const submitHandler = async (e, campaign) => {
     e.preventDefault();
   
-    const response = await fetch(`https://dematcampaignbackend.onrender.com`, {
+    const response = await fetch(`https://dematcampaignbackend.onrender.com/api/submit`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
