ee6ccd3d7728b91c0588630141453fb305d462ba
Aditya Prasad <adityak2541@gmail.com>
2025-03-10 10:45:23 +0530

    Update AngelOne.jsx

--- src/pages/AngelOne.jsx
+++ src/pages/AngelOne.jsx
@@ -12,7 +12,7 @@ const AngelOne = () => {
   const submitHandler = async (e, campaign) => {
     e.preventDefault();
   
-    const response = await fetch(`https://dematcampaignbackend.onrender.com`, {
+    const response = await fetch(`https://dematcampaignbackend.onrender.com/api/submit`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
